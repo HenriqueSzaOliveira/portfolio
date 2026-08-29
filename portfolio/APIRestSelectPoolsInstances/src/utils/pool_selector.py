a11fd426b08197fa340f2a082be47607e9bc1f94
HenriqueSzaOliveira <henrique.sza.oliveira@gmail.com>
2026-08-29 00:03:38 -0300

    ajuste logica metrica

--- portfolio/APIRestSelectPoolsInstances/src/utils/pool_selector.py
+++ portfolio/APIRestSelectPoolsInstances/src/utils/pool_selector.py
@@ -5,17 +5,22 @@ def calc_best_pool(df: pd.DataFrame, instance_type: str = None, az: str = None) 
 
     if instance_type:
         df = df[df["pool_id"].str.contains(instance_type)]
-    
+
     if az:
         df = df[df["pool_id"].str.endswith(az)]
-    
+
     if df.empty:
         return None
-    
+
     stats = (
         df.groupby("pool_id")
-        .apply(lambda g: (g["status"] == "SUCCESS").sum())
-        .reset_index(name="success_count")
+        .apply(
+            lambda g: (
+                ((g["status"] == "SUCCESS").sum()) /
+                max((g["status"] == "FAILED").sum(), 1)
+            )
+        )
+        .reset_index(name="success_ratio")
     )
 
-    return stats.loc[stats["success_count"].idxmax(), "pool_id"]
+    return stats.loc[stats["success_ratio"].idxmax(), "pool_id"]
